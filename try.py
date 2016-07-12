98c9955665cdbec121fab42607f971c8809ea332
Mamadouovo <mamadousanganaba@gmail.com>
2016-07-12 14:40:23 -0400

    square function

---
+++ try.py
@@ -0,0 +1,24 @@
+from Myro import *
+init("sim")
+penDown()
+speed=int(input("what is the speed?"))
+second=int(input("what is the second?"))
+def square():
+    penDown("red")
+    forward(speed, second)
+    turnBy(90)
+    forward(speed, second)
+    turnBy(90)
+    forward(speed, second)
+    turnBy(90)
+    forward(speed, second)
+    turnBy(90)
+    forward(speed, second)
+    penUp()
+    forward(2, 2)
+square()
+i=0
+while i<4:
+    square()
+    i=i+1
+
